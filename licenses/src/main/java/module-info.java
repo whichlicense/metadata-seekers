95f80dea28d88ac89f70730ecb00b09672a29e60
David Greven <opensource@grevend.dev>
2023-04-12 15:32:16 +0200

    Make module requirements transitive

--- licenses/src/main/java/module-info.java
+++ licenses/src/main/java/module-info.java
@@ -9,7 +9,7 @@ import com.whichlicense.metadata.seeker.MetadataSeeker;
 import com.whichlicense.metadata.seeker.license.LicenseSeeker;
 
 module whichlicense.seeker.license {
-    requires whichlicense.seeker;
+    requires transitive whichlicense.seeker;
     exports com.whichlicense.metadata.seeker.license;
     provides MetadataSeeker with LicenseSeeker;
 }
