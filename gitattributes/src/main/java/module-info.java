aa02a3416c8ce45962034f6d5431f725d6ab1c93
David Greven <opensource@grevend.dev>
2023-04-12 22:57:55 +0200

    Create GitattributesSeeker for metadata-seekers/gitattributes

---
+++ gitattributes/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/whichlicense/metadata-seekers.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import com.whichlicense.metadata.seeker.MetadataSeeker;
+import com.whichlicense.metadata.seeker.gitattributes.GitattributesSeeker;
+
+module whichlicense.seeker.gitattributes {
+    requires whichlicense.seeker;
+    exports com.whichlicense.metadata.seeker.gitattributes;
+    provides MetadataSeeker with GitattributesSeeker;
+}
